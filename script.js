fbafa31abccb734b9db92739758894cd08b2d272
João Felipe Paula de Freitas <jf.freitas77@gmail.com>
2023-10-11 04:28:52 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function mensagem() {
+    console.log("Conexão feita com sucesso!");
+
+}
+
+mensagem();
+mensagem();
+mensagem();
+
+// function imprimirMensagem() {
+//     console.log("Conexão feita com sucesso!");
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     imprimirMensagem();
+// }
